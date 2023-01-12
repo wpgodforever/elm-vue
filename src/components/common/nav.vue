1b18a3931967f6402c4b1b6b317db665fc6e118f
wuping <wuping@xinli001.com>
2023-01-12 14:44:37 +0800

    【add】封装全局顶部导航组件

---
+++ src/components/common/nav.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <van-nav-bar
+      :title="title"
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+      color="#fff"
+    >
+    <template #right v-if="rightIcon">
+      <van-icon color="#fff" :name="rightIcon" size="20" />
+    </template>
+    <template #left v-if="leftIcon">
+      <van-icon color="#fff" :name="leftIcon" size="20" />
+    </template>
+    </van-nav-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['leftIcon','rightIcon','title'],
+  methods:{
+    onClickLeft(){
+      this.$emit('onClickLeft')
+    },
+    onClickRight(){
+      this.$emit('onClickRight')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+  font-size: 16px;
+}
+.van-nav-bar {
+  background-color: #2f97ec;
+  /deep/ .van-nav-bar__title{
+    color: white;
+  }
+}
+
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="container">
-    <van-nav-bar
-      title="首页"
-    />
-    首页
+    <myNav :title="'首页'" :rightIcon="'manager-o'" :leftIcon="'search'"  @onClickLeft="onClickLeft" @onClickRight="onClickRight"></myNav>
   </div>
 </template>
 
 <script>
 export default {
-  name:'home'
+  name:'home',
+  methods:{
+    onClickLeft(){
+      console.log('左边被点击')
+    },
+    onClickRight(){
+      console.log('右边被点击')
+    }
+  }
 }
 </script>
 
@@ -17,4 +22,11 @@ export default {
 .container{
   font-size: 16px;
 }
+.van-nav-bar {
+  background-color: #2f97ec;
+  /deep/ .van-nav-bar__title{
+    color: white;
+  }
+}
+
 </style>
