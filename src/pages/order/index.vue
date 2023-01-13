0fc3f2df7f8127a908d74cb7819be534467e6ebd
wpgodforever <997587729@qq.com>
2023-01-13 16:16:41 +0800

    [add]首页静态页面完成，以及配置tabbar相关页面

---
+++ src/pages/order/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    订单
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'order',
+  data(){
+    return {
+
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/tabbar/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <tabbar></tabbar>
+  </div>
+</template>
+
+<script>
+import tabbar from '@/components/common/tabbar.vue'
+export default {
+  name: 'tabbarChoose',
+  data(){
+    return {
+    }
+  },
+  components:{
+    tabbar
+  },
+}
+</script>
