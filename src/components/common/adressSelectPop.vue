81021518e607f4c72a4610705c695acd414a1037
wpgodforever <997587729@qq.com>
2023-01-15 18:43:34 +0800

    [add]弹框添加返回按钮

--- src/components/common/adressSelectPop.vue
+++ src/components/common/adressSelectPop.vue
@@ -1,6 +1,9 @@
 <template>
-  <van-popup :value="show" position="top" @click-overlay="clickOverlay" :style="{ height: '100%' }">
+  <van-popup :value="show" position="top" :style="{ height: '100%' }">
     <div class="popContainer">
+      <div class="popContainer-header flex-align">
+        <van-icon name="arrow-left" @click="onLeftClick"/>
+      </div>
       <van-cell-group>
         <van-cell icon="location-o" title="当前城市" :value="addressInfo.name || '尚未选择'" />
       </van-cell-group>
@@ -30,11 +33,11 @@ export default {
   created(){
   },
   methods:{
-    clickOverlay () {
-      this.$emit('update:show',false)
-    },
     onHotCityClick(){
       console.log(this.allCitys,'allCitys')
+    },
+    onLeftClick(){
+      this.$emit('update:show',false)
     }
   },
   computed:{
@@ -57,9 +60,14 @@ export default {
 
 <style lang="scss" scoped>
 .popContainer{
+  .popContainer-header{
+    height: 80px;
+    font-size: 30px;
+    padding: 0 30px;
+  }
   .popContainer-hot{
     font-size: 30px;
-    padding: 20px 10px;
+    padding: 20px 40px;
   }
   .van-cell{
     font-size: 36px;
