0e77eadbb5f7e549931a334b21d7c8f5fb7cbc52
wpgodforever <997587729@qq.com>
2023-01-18 23:02:40 +0800

    [fix]css样式修复

--- src/components/common/nav.vue
+++ src/components/common/nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="navContainer">
     <van-nav-bar
       :title="title"
       @click-left="onClickLeft"
@@ -37,7 +37,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container{
+.navContainer{
   font-size: 16px;
 }
 .van-nav-bar {
